fdca35527f5c572e9245b782e2c30105654f584f
Sai <75554919+saigonu@users.noreply.github.com>
2024-01-04 17:04:45 -0800

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -18,7 +18,7 @@ const About = () => {
 <h2 className="font-medium text-3xl mb-2">I.</h2>
 
             <p className="text-gray-800 dark:text-gray-300 leading-6 tracking-wide mb-4">
-            I am a 3rd year Computer Science undergraduate who 
+            I am a 3rd year Computer Science undergraduate at UC Davis who 
             is passionate about exploring software technology and tackling complex challenges and driving meaningful impact through 
             innovative product development. I have experience with developing robust full-stack web applications and creating engaging 
             iOS applications and constantly looking for new opportunities to explore new fields and push my boundaries currently in cloud computing.
