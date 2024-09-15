154f6ca6ffef865a6289e7522cae3964ce1b045d
Sai Gonuguntla <75554919+saigonu@users.noreply.github.com>
2024-09-15 00:04:20 -0700

    fix: fix footer for now until hash is working

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,42 +1,10 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 
 const Footer = () => {
-    const [commitDate, setCommitDate] = useState<string>(new Date().toDateString());
-    const commitHash = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || "Unknown";
-
-    useEffect(() => {
-        const fetchCommitDate = async () => {
-            try {
-                const response = await axios.get(
-                    `https://api.github.com/repos/saigonu/personal-website/commits/${commitHash}`
-                );
-                const date = new Date(response.data.commit.committer.date);
-                setCommitDate(date.toDateString());
-            } catch (error) {
-                console.error("Error fetching commit date:", error);
-                setCommitDate("Unknown");
-            }
-        };
-
-        if (commitHash !== "Unknown") {
-            fetchCommitDate();
-        }
-    }, [commitHash]);
-
     return (
         <footer className="w-full flex justify-center fixed bottom-10 right-10">
             <h2 className="text-black/60 dark:text-white/30 text-sm">
-                Last updated with commit{" "}
-                <a
-                    className="text-gray-400"
-                    href={`https://github.com/saigonu/personal-website/commit/${commitHash}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    {commitHash}
-                </a>{" "}
-                on {commitDate}
+                Made with ❤️ and 🔨 by Sai Gonuguntla
             </h2>
         </footer>
     );
